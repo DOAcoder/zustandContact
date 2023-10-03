ace9c562e5c8c4256ec74ce0fb07cfad7fe0f411
DOAcoder <dadahonovorifjon808@gmail.com>
2023-10-03 07:55:43 +0500

    ok

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,16 @@
+
+import "./style.scss";
+
+const index = () => {
+    return (
+        <header>
+            <div className="container">
+                <nav className=" mt-[100px]  flex items-center justify-center h-[60px] bg-emerald-700">
+                    <h1 className="text-[40px] text-purple-800 uppercase tracking-wide font-bold">Contacts</h1>
+                </nav>
+            </div>
+        </header>
+    );
+};
+
+export default index;
